7fcd1c86095655bed7788e4dd790aa4efa6b676e
Sudip1009 <chakraborty.sudip1009@gmail.com>
2023-09-19 14:25:30 +0530

    add another field

--- Contact app/src/components/AddContact.js
+++ Contact app/src/components/AddContact.js
@@ -7,15 +7,20 @@ function AddContact() {
             <h2>Add Contact</h2>
             <form className="contact-form">
                 <div className="field">
-                    <label>Name</label>
-                    <input type="text" name="name" placeholder="Enter your name here"></input>
+                    <label>Name<span>*</span></label>
+                    <input type="text" name="name" placeholder="Enter your name here" required></input>
                 </div>
                 <div className="field">
-                    <label>Email Id</label>
-                    <input type="email" name="name" placeholder="Enter your email id here"></input>
+                    <label>Phone Number<span>*</span></label>
+                    <input type="tel" name="phone" placeholder="Enter your phone number here" pattern="[0-9]" required></input>
+                </div>
+                <div className="field">
+                    <label>Email Id<span>*</span></label>
+                    <input type="email" name="name" placeholder="Enter your email id here" required></input>
                 </div>
             </form>
-            <button className="submit">Add</button>
+            <button type="submit" value="Submit" className="submit">Add</button>
+            {/* <br></br><span>*</span>This field is required. */}
         </div>
        );
 }
